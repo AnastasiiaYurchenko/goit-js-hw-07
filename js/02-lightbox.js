6d8e7f4bbfbfe23618b057466b4b127ac1b18692
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-18 00:48:52 +0200

    Добавляє рішення 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+// console.log(galleryEl);
+
+const renderGallery = (pictures) => {
+    const result = pictures
+        .map((picture) => `<a class="gallery__item" href="${picture.original}">
+  <img class="gallery__image" src="${picture.preview}" alt="${picture.description}" />
+</a>`)
+        .join('');
+    return result;
+}
+// console.log(renderGallery(galleryItems));
+
+galleryEl.innerHTML = renderGallery(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionType: 'attr',
+    captionsData:  'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+ });
+
+
+
